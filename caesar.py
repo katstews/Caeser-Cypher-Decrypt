c63f0b252396072682735b57bb365c32c8c806db
kashew <katherinec1488@gmail.com>
2023-03-14 16:19:13 -0700

    fixed it up

--- caesar.py
+++ caesar.py
@@ -1,12 +1,15 @@
 # shift = 3
-phrase = "EVXFZO NHVMO KDIF MJVY"
+phrase = "xqkwKBN{z0bib1wv_l3kzgxb3l_70k23n2n}".upper()
+
 encrypt = []
 #ascii values must stay between 65 - 90
 #handles the 25 encryptions 
 for y in range(26):
     #x the amount to shift numbers in string
     for x in phrase:
-        if ord(x) == 32:
+        if ord(x) > 90 or ord(x) < 65:
+            encrypt.append(x)
+        elif ord(x) == 32:
             encrypt.append(" ")
         else: 
             x = chr(((ord(x) + 65) + y) % 26 + 65)
